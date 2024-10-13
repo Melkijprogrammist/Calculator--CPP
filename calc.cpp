7f7feaa66558440e2481ebeb28144f2654bacb2f
Uliana <uliana20037@yandex.ru>
2024-10-13 11:39:54 +0300

    Create calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	int sum;
+	int myltiplay;
+	int diclare;
+	int minus;
+	// Variable
+	cout << "Input variable a: ";
+	cin >> a;
+	cout << "Input variable b: ";
+	cin >> b;
+	// Sum
+	sum = a + b;
+	cout << "Sum is" << sum;
+	// *
+	myltiplay = a * b;
+	cout << "Miltyplay is" << myltiplay;
+	// /
+	diclare = a / b;
+	cout << "diclare is " << diclare;
+// -
+	minus = a - b;
+	cout << "minus is" << minus;
+}
